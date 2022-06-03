eaf86b0f5bbc8651468351cc8fb53ab929015051
Akbar Ansari <akba4ransari@gmail.com>
2022-06-03 18:36:20 +0530

    init

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Akbar\\Flutter Projects\\time_tracker_app" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Akbar\\Flutter Projects\\time_tracker_app"
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Akbar\\Flutter Projects\\time_tracker_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Akbar\\Flutter Projects\\time_tracker_app"
+  "FLUTTER_TARGET=C:\\Akbar\\Flutter Projects\\time_tracker_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Akbar\\Flutter Projects\\time_tracker_app\\.dart_tool\\package_config.json"
+)
